fbc071fddbf66316bd3804240d452b80eaaf0270
lokeshmekala <mekalalokesh1999@gmail.com>
2024-03-08 16:17:53 +0100

    updated structure and data

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,26 @@
-import React,{useState} from "react";
-import Header from "./components/header";
-import Cards from "./components/cards";
+import React from "react";
+import Header from "./components/Header/header";
+import Cards from "./components/Introduction/cards";
 import { CORE_CONCEPTS } from "../data";
-import { EXAMPLES } from "../data";
-import TabButton from "./components/tabButton";
+import Explaination from "./components/Explaination/explaination";
+
 
 
 function App() {
-  const [tabContent,setContent]=useState('')
-
-  function handleClick(element){
-    setContent(element)
-  }
+  
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
-          <h2>Core Concepts</h2>
+          <h2>Focused Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((conceptelements,id)=>
               <Cards {...conceptelements}/>
             )}
           </ul>
         </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton clickEvent={()=>handleClick("components")}>Components</TabButton>
-            <TabButton clickEvent={()=>handleClick("jsx")}>JSX</TabButton>
-            <TabButton clickEvent={()=>handleClick("props")}>Props</TabButton>
-            <TabButton clickEvent={()=>handleClick("state")}>State</TabButton>
-          </menu>
-            {!tabContent?<p>Please choose a topic</p> : 
-            (<div id="tab-content"><h3>{EXAMPLES[tabContent].title}</h3><p>{EXAMPLES[tabContent].description}</p><pre>
-                <code>
-                  {EXAMPLES[tabContent].code}
-                </code>
-              </pre></div>)
-            }
-
-        </section>
+        <Explaination/>
       </main>
     </div>
   );
---
+++ src/components/Explaination/explaination.jsx
@@ -0,0 +1,37 @@
+import React,{useState} from "react";
+import { EXAMPLES } from "../../../data";
+import TabButton from "./tabButton";
+
+import './explaination.css'
+const Explaination= () =>{
+
+    const [tabContent,setContent]=useState('');
+    function handleClick(element){
+        setContent(element)
+    }
+
+    return(
+        <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+          <TabButton clickEvent={()=>handleClick("dynamicprograming")}>Dynamic Programing</TabButton>
+          <TabButton clickEvent={()=>handleClick("linkedlist")}>Linked Lists</TabButton>
+          <TabButton clickEvent={()=>handleClick("trees")}>Trees</TabButton>
+          <TabButton clickEvent={()=>handleClick("graphs")}>Graphs</TabButton>
+        </menu>
+          {!tabContent?<p>Please choose a topic</p> : 
+          (<div id="tab-content">
+            <h3>{EXAMPLES[tabContent].title}</h3>
+            <p>{EXAMPLES[tabContent].description}</p>
+            <p>{EXAMPLES[tabContent].representation}</p>
+            <pre className="preformating">
+              <code>
+                {EXAMPLES[tabContent].code}
+              </code>
+            </pre>
+          </div>)
+          }
+      </section>
+    );
+}
+export default Explaination;
